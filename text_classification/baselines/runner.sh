d1d8f4fd7fe0fe365dd5293a2413725cda1d4df4
jiayuanx <jiayuanx@andrew.cmu.edu>
2020-04-22 19:18:07 -0400

    move to AWS

--- text_classification/baselines/runner.sh
+++ text_classification/baselines/runner.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# run all
 # python3 train.py config_20ng_lstm.yaml
 # python3 test.py config_20ng_lstm.yaml
 
 # python3 train.py config_20ng_tfmr.yaml
-python3 test.py config_20ng_tfmr.yaml
+# python3 test.py config_20ng_tfmr.yaml
 
 # python3 train.py config_mr_lstm.yaml
 # python3 test.py config_mr_lstm.yaml
 
 # python3 train.py config_mr_tfmr.yaml
 # python3 test.py config_mr_tfmr.yaml
+
+# hyper-param tuning
+python3 train.py model_tuning/config_20ng_tfmr1.yaml
+python3 test.py model_tuning/config_20ng_tfmr1.yaml
+
+python3 train.py model_tuning/config_20ng_tfmr2.yaml
+python3 test.py model_tuning/config_20ng_tfmr2.yaml
+
+python3 train.py model_tuning/config_20ng_tfmr3.yaml
+python3 test.py model_tuning/config_20ng_tfmr3.yaml
+
+python3 train.py model_tuning/config_20ng_tfmr4.yaml
+python3 test.py model_tuning/config_20ng_tfmr4.yaml
